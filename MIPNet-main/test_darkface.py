6fa9d48882e1f13f6370ab6bb1a842fe8bac2d22
qian zhao <167187705+dlgxwcvhehks@users.noreply.github.com>
2026-03-08 23:05:56 +0800

    Add files via upload

--- MIPNet-main/test_darkface.py
+++ MIPNet-main/test_darkface.py
@@ -1,1 +1,71 @@
+import os
 
+import torch
+import detectron2.utils.comm as comm
+from detectron2.checkpoint import DetectionCheckpointer
+from detectron2.config import get_cfg
+from detectron2.data import build_detection_test_loader
+from detectron2.data.datasets import register_coco_instances
+from detectron2.engine import DefaultTrainer, default_argument_parser, default_setup, launch
+from detectron2.evaluation import COCOEvaluator, verify_results
+
+from queryrcnn import SparseRCNNDatasetMapper, add_sparsercnn_config
+
+
+class Trainer(DefaultTrainer):
+    @classmethod
+    def build_evaluator(cls, cfg, dataset_name, output_folder=None):
+        if output_folder is None:
+            output_folder = os.path.join(cfg.OUTPUT_DIR, "inference")
+        return COCOEvaluator(dataset_name, cfg, True, output_folder)
+
+    @classmethod
+    def build_test_loader(cls, cfg, dataset_name):
+        mapper = SparseRCNNDatasetMapper(cfg, is_train=False)
+        return build_detection_test_loader(cfg, dataset_name, mapper=mapper)
+
+
+def setup(args):
+    cfg = get_cfg()
+    add_sparsercnn_config(cfg)
+    cfg.merge_from_file(args.config_file)
+    cfg.merge_from_list(args.opts)
+    cfg.freeze()
+    default_setup(cfg, args)
+    return cfg
+
+
+def main(args):
+    cfg = setup(args)
+    torch.cuda.empty_cache()
+
+    register_coco_instances(
+        "darkface_test", {}, "data/darkface_test.json", "data/darkface/test/"
+    )
+
+    if not args.eval_only:
+        raise ValueError("This script is intended for evaluation only. Please use --eval-only.")
+
+    model = Trainer.build_model(cfg)
+    DetectionCheckpointer(model, save_dir=cfg.OUTPUT_DIR).resume_or_load(
+        cfg.MODEL.WEIGHTS, resume=args.resume
+    )
+
+    res = Trainer.test(cfg, model)
+
+    if comm.is_main_process():
+        verify_results(cfg, res)
+    return res
+
+
+if __name__ == "__main__":
+    args = default_argument_parser().parse_args()
+    print("Command Line Args:", args)
+    launch(
+        main,
+        args.num_gpus,
+        num_machines=args.num_machines,
+        machine_rank=args.machine_rank,
+        dist_url="auto",
+        args=(args,),
+    )
